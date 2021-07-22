102efb7ebafc64309820b633c2f942ba2bd64232
Rafael Bertoldo <jagoxitz@gmail.com>
2021-07-22 00:25:45 -0300

    feat: landing page complete

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,29 @@
+import { useHistory } from "react-router-dom";
+import Button from "../../components/Button";
+import { Container, Content } from "./style";
+
 function Home() {
-  return <div>Home</div>;
+  const history = useHistory();
+  const handleNavigation = (path) => {
+    return history.push(path);
+  };
+
+  return (
+    <Container>
+      <Content>
+        <h1>
+          do<span>.</span>it
+        </h1>
+        <span>Organize-se de forma fácil e efetiva</span>
+        <div>
+          <Button onClick={() => handleNavigation("/signup")} whiteSchema>
+            Cadastre-se
+          </Button>
+          <Button onClick={() => handleNavigation("/login")}>Login</Button>
+        </div>
+      </Content>
+    </Container>
+  );
 }
 
 export default Home;
